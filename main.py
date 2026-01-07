8d6af906814385b555107f87176bb00c25fd5a38
Erik Tatsumi Leutz <erik.leutz@example.com>
2026-01-07 23:05:33 +0100

    Updating lifeexpectancy and updating main

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from modules.nikolaos import nikolaos_func
 from modules.main import main_dash_app
 
 row_data_route = "cleaning/life expected.csv"
-cleaned_data_route = 'life_cleaned.csv'
-region_data_route = 'country_continent.csv'
+cleaned_data_route = 'cleaned_data.csv'
+#region_data_route = 'country_continent.csv'
 
-generate_cleaned_data(row_data_route, 
-                        cleaned_data_route,
-                        machine_noise_floor=1e-12)
+# generate_cleaned_data(row_data_route, 
+#                         cleaned_data_route,
+#                         machine_noise_floor=1e-12)
 
 def whole_team(cleaned_data_route, return_len: int = 10, country_index: int=0, L_or_S: str='short'):
     si_yu_plot(cleaned_data_route)
@@ -20,7 +20,7 @@ def whole_team(cleaned_data_route, return_len: int = 10, country_index: int=0, L
 
     prakriti_func(cleaned_data_route)
 
-    nikolaos_func(cleaned_data_route, region_data_route) # open the html file in iframe_figures folder
+    #nikolaos_func(cleaned_data_route, region_data_route) # open the html file in iframe_figures folder
 
     return alert_package, alert_year_for_country
 
